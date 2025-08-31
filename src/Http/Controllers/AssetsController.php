4b7370c47c1fd6c1926ec688e0e54953febc76cd
Fa-BRAIK <faroukbraik17@gmail.com>
2025-08-31 10:58:47 -0400

    patch: fixed assets controller not returning file

--- src/Http/Controllers/AssetsController.php
+++ src/Http/Controllers/AssetsController.php
@@ -2,9 +2,9 @@
 
 namespace Nuxtifyts\DashStackTheme\Http\Controllers;
 
-use Illuminate\Http\Response;
 use Illuminate\Http\File;
-use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Throwable;
 
 readonly class AssetsController
@@ -15,20 +15,17 @@ readonly class AssetsController
 
     protected const EXPIRES = '+1 year';
 
-    public function __invoke(string $fileName): Response
+    public function __invoke(string $fileName): BinaryFileResponse
     {
         try {
-            $file = new File($filePath = __DIR__."/../../../resources/assets/{$fileName}");
+            $filePath = __DIR__."/../../../resources/assets/{$fileName}";
 
-            return response(
-                content: $file->getContent(),
-                headers: [
-                    'Content-Type' => "application/{$file->extension()}",
+            return response()
+                ->file($filePath, headers: [
                     'Expires' => sprintf('%s GMT', gmdate('D, d M Y H:i:s', strtotime(static::EXPIRES))),
                     'Cache-Control' => static::CACHE_CONTROL,
                     'Last-Modified' => sprintf('%s GMT', gmdate('D, d M Y H:i:s', filemtime($filePath))),
-                ]
-            );
+                ]);
         } catch (Throwable) {
             abort(Response::HTTP_NOT_FOUND);
         }
